2728cf995d5a4ca02b0241e5dad0930809da5d74
ashwinmali7781 <ashwinmali72@gmail.com>
2026-04-26 16:41:40 +0530

    email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,26 @@
+import { APIError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Ronaldo",
-    // message: "ok",
+  const { fullname, email, username, password } = req.body;
+  console.log("email:", email);
+
+  if (
+    [fullname, email, username, password].some(
+      (field) => field.trim?.trim() === ""
+    )
+  ) {
+    throw new APIError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }, { password }],
   });
+
+  if (existedUser) {
+    throw new APIError(409, "User with email or username already exists");
+  }
 });
 
 export { registerUser };
