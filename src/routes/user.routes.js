e468041edbea7c747e6853f5ea7ec7a07cd8160a
ashwinmali7781 <ashwinmali72@gmail.com>
2026-04-26 12:47:39 +0530

    async

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,6 @@ import { registerUser } from "../controllers/user.controller";
 
 const router = Router();
 
-router.route("/resgister").post(registerUser);
+router.route("/register").post(registerUser);
 
 export default router;
