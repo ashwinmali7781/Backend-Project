fde777ca61fed263d3e8d2eeb5dadc40abfff19f
ashwinmali7781 <ashwinmali72@gmail.com>
2026-04-28 01:23:39 +0530

    user controller update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,19 +3,19 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { fullname, email, username, password } = req.body;
+  const { fullName, email, username, password } = req.body;
   console.log("email:", email);
 
   if (
-    [fullname, email, username, password].some(
-      (field) => field.trim?.trim() === ""
+    [fullName, email, username, password].some(
+      (field) => field?.trim() === ""
     )
   ) {
     throw new APIError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
-    $or: [{ username }, { email }, { password }],
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }]
   });
 
   if (existedUser) {
